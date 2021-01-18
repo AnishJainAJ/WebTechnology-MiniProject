050a9daed17b5e639cb76c9bae175ac56ef5ebe6
AnishJainAJ <47848917+AnishJainAJ@users.noreply.github.com>
2021-01-18 15:51:10 +0530

    Add files via upload

---
+++ use_site.php
@@ -0,0 +1,50 @@
+
+
+
+<?php include('header.php'); ?>
+
+  <body id="home">
+  <br />
+  <?php  include('navbar_index.php');  ?>
+   
+  <div class="container-fluid">
+  
+ 
+  
+  
+<div class="row">
+   
+  <div class="col-md-12">
+
+	<!-- Modal -->
+	<div   id="use" tabindex="-1" >
+		<div class="modal-dialog">
+			<div class="modal-content">
+			<div class="modal-header">
+				<a href="index.php" type="button" class="close"  >&times;</a>
+				<h4 class="modal-title" id="myModalLabel">Use of the Site</h4>
+			</div>
+			<div class="modal-body">
+			<p>
+			This site must be used only by VVCE students and professors for professional use.<br>Thank You.
+</p>
+			</div>
+			<div class="modal-footer">
+				<a href="index.php" type="button" class="btn btn-default"  ><i class="fa fa-times"></i> Close</a>
+				
+			</div>
+			</div>
+			
+		</div>
+		</div>	 
+        
+  </div>
+ 
+</div>
+ 
+  </div>
+  
+ 
+  </body>
+ 
+ 
